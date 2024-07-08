eac3adf1e484a7701c009b0fe7f213bf6b26755d
Deepaksharma-39 <d33paksharma.39@gmail.com>
2024-07-08 10:44:12 +0530

    new privacy

--- frontend/src/pages/Privacy.js
+++ frontend/src/pages/Privacy.js
@@ -6,74 +6,75 @@ const Privacy = () => {
         <h3>Privacy Policy</h3>
 
         <p>
-        PRIVACY POLICY FOR MOBILE APP OF VEHICLE STATE  TAX Thanks for visiting our App (VEHICLE STATE  TAX) and viewing our privacy policy, kindly read it  conscientiously in order to unfold the data  operation and its uses. We only collect necessary  and minimum information which are required for the  service operation, so that you can avail our services  hurdle free. We in the current section will get  through how we handle and collect information  while entering and availing service on our App.
-        
-         IV.  
+        PRIVACY POLICY FOR VEHICLE STATE TAX APP
         </p>
 
         <p>
-        1  SCOPE: The sole purpose of this Privacy Policy is to  protect the data of its users and to disclose what  data we possessed in exchange of our services. App  will use personal information in order to function  properly. This App does not automatically capture  any specific personal information from you, (like  name, phone number, or e-mail address), that  allows us to identify you individually. Wherever the  App requests you to provide personal information,  you will be informed of the particular purposes for  which the information is gathered and adequate  security measures will be taken to protect your  personal information. 
+        Thanks for visiting our App (ALL TAX SOLUTIONS) and viewing our privacy policy. Kindly read it conscientiously to understand the data operation and its uses. We only collect the necessary and minimum information required for service operation, so you can avail our services hurdle-free. This section will explain how we handle and collect information while you enter and avail services on our App.
         </p>
 
         <p>
-        2. INFORMATION WE COLLECT  FROM YOU:
+        1. SCOPE: The sole purpose of this Privacy Policy is to protect user data and disclose what data we possess in exchange for our services. The app requires personal information to function properly. This app does not automatically capture any specific personal information from you, like your name, phone number, or e-mail address, that allows us to identify you individually. Wherever the app requests you to provide personal information, you will be informed of the particular purposes for which the information is gathered and adequate security measures will be taken to protect your personal information.
+        </p>
+
+        <p>
+        2. INFORMATION WE COLLECT FROM YOU:
         </p>
 
         <ol>
             <li>
-            Personal Data We collect the  information you give us while using our services.  Personal Information for the privacy policy is any  information that directly or indirectly identifies you,  which is included but not limited; a) Your Name, and  other personal information such as; b) Contact  Details(Phone Number); c) Visiting State; d) Border  District Through Entering; e) Vehicle Number; f) Tax  Mode (Days/Quarter); g) Payment Mode. h) IP  Address i)Location.
-            </li>
-
-            <li>
-            Location Information The app  collects location information while availing services.  It collects location information for two purposes. We collect information about the Border State/District,  where the vehicle previously or currently located  (Barrier/Border State or District) and to the State  that you intend to visit (Visiting State). Apart from  that, third parties may access your live location  during your payment process.
-            </li>
-
-            <li>
-            Financial  Information We collect some important financial  information such as Account Holder's Name, UPI ld,  amount transferred and sometimes Mobile no.  which depends on third-party payment gateway  which are necessary for the execution of tax  payment and other mandatory compliances. We do  not collect any sensitive data such as Debit/Credit  card details, password or any financial information  as such other than mentioned above. This policy  DOES NOT apply to information that you provide to,  or that is collected by any third-party or through any  payment gateways as intimated above. VEHICLE  STATE TAX encourages you to consult directly with  such third-parties about their privacy practices.
-            </li>
-
-            <li>
-            Cookies . Your device has the in-built facility for  storing "cookies" - that hold information that allows  App to recognize your preferences such as autofill  as mentioned hereabove. Our App takes advantage  of this facility to enhance your experience. A cookie  is a small amount of data that is sent to your App's  device and stored on your device's hard drive. .  When you browse through our app, we may collect  information regarding the mobile/ tab device  details, domain, and host from which you access the  internet, the Internet Protocol [IP] address of the  device or Internet service provider [ISP] you are  using, and anonymous site statistical data. . ost  devices automatically accept cookies as the default  settina You can modifv vour devices' settinas to reject our cookies or to prompt you before  accepting a cookie by editing your devices' software  options. However, if a software/application is set  not to accept cookies or if a user rejects a cookie,  some portions of the Services may not function  properly.
+            Personal Data: We collect the information you give us while using our services. Personal Information for this privacy policy is any information that directly or indirectly identifies you, which is included but not limited to: a) Your Name; b) Vehicle Number.
             </li>
         </ol>
 
-
         <p>
-        USE OF INFORMATION: 
+        USE OF INFORMATION:
         </p>
 
         <ul>
             <li>
-            Operational  Purposes We collect personal data for the purpose  of providing hassle free services and to improve the  functionality and user-friendliness of our App for  future requirements. This is necessary for our  legitimate interests to constantly monitor and  improve our online presence and services to you. a)  These information's can be collected when you  download or use our application. If you contact us  directly, e may receive additional information  about you such as your name, email address, phone  number, the contents of the message and/or  attachments you may send us, and any other  information you may choose to provide. b) The  details shared by the user will be used to create tax  slips for payment of taxes levied by State  government for their respective vehicles for the  purpose border tax payment. We precisely use your  personal data which includes Your vehicle no.,  Border and Visiting State, and your Name. c) Some  other information can also be collected as when it is  required for the operation of the app in order to  provide quality services and same will be intimated  beforehand in our Privacy Policy which is listed and  not limited to.
+            Operational Purposes: We collect personal data to provide hassle-free services and improve the functionality and user-friendliness of our app for future requirements. This is necessary for our legitimate interests to constantly monitor and improve our online presence and services to you. a) This information is collected when you download or use our application. b) The details shared by the user will be used to create tax slips for payment of taxes levied by the State government for their respective vehicles for the purpose of border tax payment. We precisely use your personal data, which includes your vehicle number and your name.
             </li>
 
             <li>
-            Administrative Purposes This app  may use Personal Information about you for its  administrative purposes, including to: measure  interest in our Services; improve our Services and  develop new services; enforce the Terms that  govern our Services or this Privacy Policy; protect ur Services, systems, information, employees,  business partners, service providers, users,  customers or others; verify Individual identity:  prevent potentially prohibited or illegal activities;  and process payment for Services purchased. •  Contract: When you subscribe to the Services, we  need to process some of your personal information  such as name, payment information, address and  email in order to provide you with the agreed  Services. • Legitimate interest: We may rely on a  legitimate interest when processing personal  information. We collect information related to  emergencies and incidents and communications  that occur during and after such events, we do this  for the purposes of ensuring the safety of our users.  • Legal obligation: We may be required to use your  personal information to comply with the law. •  Consent: We might ask for your consent to use your  information for specific purposes, like news and  offers from our collaborating partners.
+            Administrative Purposes: This app may use personal information about you for its administrative purposes, including: measuring interest in our services; improving our services and developing new services; enforcing the terms that govern our services or this privacy policy; protecting our services, systems, information, employees, business partners, service providers, users, customers, or others; verifying individual identity; preventing potentially prohibited or illegal activities; and processing payments for services purchased.
             </li>
 
             <li>
-            Emergency Purposes This App only collects and use  your personal information only when you use the  services of the app. There may be some situations  where we may have to share your personal  information with third parties without getting your  prior consent. Such third parties will be limited to: •  Police • Ambulances • Fire Brigade • Judicial or  Quasi-Judicial Authorities. The information which  are shared by the above authorities are shared in  compliance with any law, rule, regulation or  directions by the Government of India, Union  Territories, State or any other local authority under  the laws of India.
+            Emergency Purposes: This app only collects and uses your personal information when you use the services of the app. There may be some situations where we may have to share your personal information with third parties without getting your prior consent. Such third parties will be limited to: Police, Ambulances, Fire Brigade, Judicial or Quasi-Judicial Authorities. The information shared with the above authorities is in compliance with any law, rule, regulation, or directions by the Government of India, Union Territories, State, or any other local authority under the laws of India.
             </li>
 
             <li>
-            Marketing and Advertising  Purposes We may use your personal information in  order to improve our marketing and promotional efforts, to analyse usage of our app, to improve our  content and services. This ensures a smooth,  efficient, safe and user-friendly experience while  using our app.
-            </li>
-
-            <li>
-            THIRD PARTY LIABILITY: We do not  sell or share any personally identifiable information  volunteered on the App to any third party (public/  private) except in the cases mentioned under  Clause (3)(I)(b) and 3(Il1). If you encounter any  redirection to any third-party platform from our app,  in that case we will not be responsible for any data  theft, injury and/or mishappening. We Vehicle State  Tax, urge you to be kindly aware that when you leave  our app through redirection or/and hyperlink, other  sites or apps may have different privacy policies and  terms which are beyond our control. Please be sure  to check the Privacy Policies of these sites as well  as their "Terms of Service" before engaging in any  business or uploading any information.
-            </li>
-
-            <li>
-            RETENTION OF YOUR DATA: We may retain data  provided by User for as long as they use the  Application and for a reasonable time thereafter. We  may retain Automatically Collected information also  for a reasonable period of time depending on the  nature of application and thereafter may store it in  aggregate. All personal information collected may  be retained on the mobile device for a period of 12  months from the date of collection after which, if it  has not already been uploaded to the Server, will be  purged from the App.
-            </li>
-
-            <li>
-            DATA SECURITY: Any  information provided to our App will be protected  from loss, misuse, nauthorized access or  disclosure, alteration, or destruction. We gather  certain information about the User, such as Internet  protocol (IP) addresses, domain name, browser  type, operating system. We make no attempt to link these addresses with the identity of individuals  visiting our App unless an attempt to damage the  App has been detected. Except for authorised law  enforcement investigations, no other attempts are  made to identify individual users or their usage  habits. Raw data logs are used for no other  purposes and are scheduled for regular deletion.  Unauthorised attempts to upload information or  change information on this service are strictly  prohibited and may be punishable under the Indian  IT Act.  
+            Marketing and Advertising Purposes: We may use your personal information to improve our marketing and promotional efforts, analyze usage of our app, and improve our content and services. This ensures a smooth, efficient, safe, and user-friendly experience while using our app.
             </li>
         </ul>
 
         <p>
-        CHANGES IN PRIVACY POLICY: This Privacy  Policy may change from time to time. You  understand and agree that you will be deemed to  have accepted the updated Privacy Policy if you use  the Services after the updated Privacy Policy which  will be posted on the Services. If at any point you do  not agree to any portion of the Privacy Policy then in  effect, you must immediately stop using the  Services. i. Revision to the Privacy Policy We may  revise this Privacy Policy in our sole discretion, so  review it periodically. If you continue to using this  app and use the services made available to you after  such changes have been made, you hereby provide  your consent to the changes. ii. Posting of Revised  Privacy Policy If there are any material changes to  this Privacy Policy, VEHICLE STATE TAX may notify  you by email or as otherwise required by applicable  law. We will post any adjustments to the Privacy  Policy on this app, and the revised version will be  effective immediately when it is posted (or upon  notice as applicable). If you are concerned about  how your information is used, bookmark this page  and read this Privacy Policy periodically,  GRIEVANCES: If you have any queries about our  Privacy Policy, or if you want to know what 
+        THIRD PARTY LIABILITY: We do not sell or share any personally identifiable information volunteered on the app to any third party (public/private) except in the cases mentioned under Clause (3)(I)(b) and (3)(III). If you encounter any redirection to any third-party platform from our app, we will not be responsible for any data theft, injury, and/or mishap. We, ALL TAX SOLUTIONS, urge you to be aware that when you leave our app through redirection or hyperlink, other sites or apps may have different privacy policies and terms that are beyond our control. Please be sure to check the privacy policies of these sites and their "Terms of Service" before engaging in any business or uploading any information.
+        </p>
+
+        <p>
+        RETENTION OF YOUR DATA: We may retain data provided by the user for as long as they use the application and for a reasonable time thereafter. We may retain automatically collected information also for a reasonable period of time depending on the nature of the application and may store it in aggregate form. All personal information collected may be retained on the mobile device for 12 months from the date of collection after which, if it has not already been uploaded to the server, will be purged from the app.
+        </p>
+
+        <p>
+        DATA SECURITY: Any information provided to our app will be protected from loss, misuse, unauthorized access or disclosure, alteration, or destruction. We gather certain information about the user, such as Internet Protocol (IP) addresses, domain name, browser type, and operating system. We make no attempt to link these addresses with the identity of individuals visiting our app unless an attempt to damage the app has been detected. Except for authorized law enforcement investigations, no other attempts are made to identify individual users or their usage habits. Raw data logs are used for no other purposes and are scheduled for regular deletion. Unauthorized attempts to upload information or change information on this service are strictly prohibited and may be punishable under the Indian IT Act.
+        </p>
+
+        <p>
+        CHANGES IN PRIVACY POLICY: This privacy policy may change from time to time. You understand and agree that you will be deemed to have accepted the updated privacy policy if you use the services after the updated privacy policy is posted on the services. If at any point you do not agree to any portion of the privacy policy then in effect, you must immediately stop using the services.
+        </p>
+
+        <p>
+        i. Revision to the Privacy Policy: We may revise this privacy policy in our sole discretion, so review it periodically. If you continue to use this app and the services made available to you after such changes have been made, you hereby provide your consent to the changes.
+        </p>
+
+        <p>
+        ii. Posting of Revised Privacy Policy: If there are any material changes to this privacy policy, VEHICLE STATE TAX may notify you by email or as otherwise required by applicable law. We will post any adjustments to the privacy policy on this app, and the revised version will be effective immediately when it is posted (or upon notice as applicable). If you are concerned about how your information is used, bookmark this page and read this privacy policy periodically.
+        </p>
+
+        <p>
+        GRIEVANCES: If you have any queries about our privacy policy, or if you want to know what information we have collected about you, please contact us at alltaxsolutions44@gmail.com.
         </p>
     </div>
   )
